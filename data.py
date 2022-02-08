a276f35650fb333fe1cf1c6b075903df708f0822
David Andrew Miller <davidmillerandrew@gmail.com>
2022-02-08 20:56:18 +0100

    Added Test Data and Art

--- data.py
+++ data.py
@@ -10,204 +10,60 @@ and there Data Attributes here
 background = [
     {
         "file": "background_1",
-        "label": "Cloud Pink",
-        "weight": 10,
+        "label": "Checker",
+        "weight": 30,
     },
     {
         "file": "background_2",
-        "label": "Much Wow Cloud",
-        "weight": 8,
-    },
-    {
-        "file": "background_3",
-        "label": "Intruder Cells",
-        "weight": 8,
-    },
-    {
-        "file": "background_4",
-        "label": "Hereeeeee",
-        "weight": 8,
-    },
-    {
-        "file": "background_5",
-        "label": "Dark Luv",
-        "weight": 8,
-    },
-    {
-        "file": "background_6",
-        "label": "Famous Artiest",
-        "weight": 8,
-    },
-    {
-        "file": "background_7",
-        "label": "Linear Smiley",
-        "weight": 8,
-    },
-    {
-        "file": "background_8",
-        "label": "Radial Smiley",
-        "weight": 8,
-    },
-    {
-        "file": "background_9",
-        "label": "3 AM",
+        "label": "Gradient",
         "weight": 10,
     },
     {
-        "file": "background_10",
-        "label": "Through Spiral",
-        "weight": 8,
+        "file": "background_3",
+        "label": "Solid",
+        "weight": 60,
     },
-    {
-        "file": "background_11",
-        "label": "Frames of useless Lives",
-        "weight": 8,
-    },
-    {
-        "file": "background_12",
-        "label": "Nothing you can do",
-        "weight": 8,
-    },
+
 ]
 
 body = [
     {
         "file": "body_1",
-        "label": "Durty Blondie",
-        "weight": 14,
+        "label": "Green",
+        "weight": 45,
     },
     {
         "file": "body_2",
-        "label": "Guy that hates you",
-        "weight": 8,
+        "label": "Blue",
+        "weight": 25,
     },
     {
         "file": "body_3",
-        "label": "Guy you hate",
-        "weight": 8,
-    },
-    {
-        "file": "body_4",
-        "label": "You know who",
-        "weight": 14,
-    },
-    {
-        "file": "body_5",
-        "label": "Let's do this",
-        "weight": 4,
-    },
-    {
-        "file": "body_6",
-        "label": "Have you heard of Roko's Basilisk?",
-        "weight": 3,
-    },
-    {
-        "file": "body_7",
-        "label": "Yesterday's chick",
-        "weight": 8,
-    },
-    {
-        "file": "body_8",
-        "label": "Uncle form down under - the Bridge",
-        "weight": 10,
-    },
-    {
-        "file": "body_9",
-        "label": "Stuck in olden Times",
-        "weight": 8,
-    },
-    {
-        "file": "body_10",
-        "label": "Don't argue with her",
-        "weight": 11,
-    },
-    {
-        "file": "body_11",
-        "label": "Matrix might be real",
-        "weight": 3,
-    },
-    {
-        "file": "body_12",
-        "label": "Shamisen is cool",
-        "weight": 4,
-    },
-    {
-        "file": "body_13",
-        "label": "Projection Loop",
-        "weight": 5,
+        "label": "Purple",
+        "weight": 30,
     },
 ]
 
 face = [
     {
         "file": "face_1",
-        "label": "Still better then you",
-        "weight": 9,
-    },
-    {
-        "file": "face_2",
-        "label": "Got bit, or bit something",
-        "weight": 8,
-    },
-    {
-        "file": "face_3",
-        "label": "Nandor",
-        "weight": 9,
-    },
-    {
-        "file": "face_4",
-        "label": "OuO",
-        "weight": 6,
-    },
-    {
-        "file": "face_5",
-        "label": "Describes your existence",
-        "weight": 11,
-    },
-    {
-        "file": "face_6",
-        "label": "Existence x Drugs",
+        "label": "Bold Smile",
         "weight": 10,
     },
     {
-        "file": "face_7",
-        "label": "'british 'innit",
-        "weight": 9,
+        "file": "face_2",
+        "label": "Happy",
+        "weight": 25,
     },
     {
-        "file": "face_8",
-        "label": "Dali's reincarnation",
-        "weight": 4,
+        "file": "face_3",
+        "label": "Smiley",
+        "weight": 60,
     },
     {
-        "file": "face_9",
-        "label": "Has at least one body in the fridge",
-        "weight": 9,
-    },
-    {
-        "file": "face_10",
-        "label": "B-aus",
-        "weight": 8,
-    },
-    {
-        "file": "face_11",
-        "label": "**gg**",
-        "weight": 9,
-    },
-    {
-        "file": "face_12",
-        "label": "SCP #[REDUCTED]",
-        "weight": 3,
-    },
-    {
-        "file": "face_13",
-        "label": "Kawai-Chan",
-        "weight": 4,
-    },
-    {
-        "file": "face_14",
-        "label": "Deep Fried",
-        "weight": 1,
+        "file": "face_4",
+        "label": "Teeth Smile",
+        "weight": 5,
     },
 ]
 
@@ -215,36 +71,16 @@ hat = [
     {
         "file": "",
         "label": "No Hat",
-        "weight": 35,
+        "weight": 60,
     },
     {
         "file": "hat_1",
-        "label": "Crown",
-        "weight": 4,
+        "label": "Cylinder",
+        "weight": 25,
     },
     {
         "file": "hat_2",
-        "label": "Yeehaw Hat",
-        "weight": 14,
-    },
-    {
-        "file": "hat_3",
-        "label": "Collapsing House with a Spike",
-        "weight": 14,
-    },
-    {
-        "file": "hat_4",
-        "label": "Akai bōshi o kabutta wāmu",
-        "weight": 8,
-    },
-    {
-        "file": "hat_5",
-        "label": "Half Clowns Hat",
+        "label": "Joker",
         "weight": 15,
-    },
-    {
-        "file": "hat_6",
-        "label": "Spiky Hat",
-        "weight": 10,
     },
 ]
